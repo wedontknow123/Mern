994807fa3057f34f19a32cbd918feb3edd86af9f
sourish <sourish994@gmail.com>
2020-06-24 00:27:45 +0530

    key adjustment

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,11 @@
 exports.dbConfig={
-    server:"LAPTOP-P4D7V446\\SQLEXPRESS",
+    //server:"LAPTOP-P4D7V446\\SQLEXPRESS",
+    server:"LAPTOP-DVJ39MSC",
     database:"INTERNSHIP",
-    user:"tejasp",
-    password:"tejasp@2000",
+    //user:"tejasp",
+    user:"sourish",
+    //password:"tejasp@2000",
+    password:"sourish@123",
     port:1433
 };
 exports.httpMsgsFormat="HTML";
