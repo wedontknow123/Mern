c0bd118d82d96c4f32ca70c74a9b8815ff67c204
tejasp2k <tejasp2k@gmail.com>
2020-07-11 03:18:25 +0530

    hmm

--- routes/api/docfiles.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router =express.Router();
-const call= require('./calling');
-
-router.get('/',(req,res)=>{
-    call.get9(req,res);
-})
-
-module.exports=router;
