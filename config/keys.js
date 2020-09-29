89c2f0a31812cea2b0f34fde6cb394df7116ebff
sourish <sourish994@gmail.com>
2020-09-29 13:09:09 +0530

    bleh

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,8 @@
 exports.dbConfig={
     server:"LAPTOP-P4D7V446\\SQLEXPRESS",
-    //server:"LAPTOP-DVJ39MSC",
     database:"INTERNSHIP",
-    user:"tejasp",
-    //user:"sourish",
-    password:"tejasp@2000",
-    //password:"sourish@123",
+    user:"sourish",
+    password:"sourish@123",
     port:1433
 };
 exports.httpMsgsFormat="HTML";
